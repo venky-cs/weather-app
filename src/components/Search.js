baa73902f0a8b7edc91c632dcb8743a84c66db10
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-03-01 21:22:25 +0530

    'create-searchbar'

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react';
+
 
 function Search() {
+    const [search, setSearch] = useState(true)
     return (
       <div className="search">
-        <div className="top">
-          <button>Search for places</button>
-          <button>&#9728;</button>
-        </div>
+        {search ? (
+          <div className="top">
+            <button onClick={() => setSearch((prevState) => !prevState)}>
+              Search for places
+            </button>
+            <button>&#9728;</button>
+          </div>
+        ) : (
+          <div className="searchbar">
+            <button onClick={() => setSearch((prevState) => !prevState)}>X</button>
+            <div className="top">
+              <input type="text" name="" placeholder="search location" />
+              <button>Search</button>
+            </div>
+          </div>
+        )}
       </div>
     );
+
+    function searchbar() {
+        
+    }
 }
 
 export default Search
