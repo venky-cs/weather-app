a80dffd4f4184b7e2306a26d98e92b21693b4636
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-03-03 01:48:34 +0530

    'fetch-woied'

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,38 +1,40 @@
-import React,{useState,useEffect} from 'react'
-import SideBar from './SideBar';
-import DetailBar from './DetailBar';
+import React, { useState, useEffect } from "react";
+import SideBar from "./SideBar";
+import DetailBar from "./DetailBar";
 
-import axios from 'axios';
+import axios from "axios";
 
+let city = 28743736;
 function Main() {
-    const [state, setState] = useState('');
-    useEffect(() => {
-        console.log(state)
-        if(state.length > 1){
-            axios
-            .get(
-                ` https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${state}`
-                )
-          .then((response) => console.log(response.data));
-        }else{
-            console.log('empty state')
-        }
-        }, [state])
-    return (
-        <div className="main">
-            <SideBar result={getLocation}/>
-            <DetailBar value={state}/>
-        </div>
-    )
-
-    async function getLocation(value) {
-        let a = await value;
-        if(a.length > 1){
-            setState(a)
-        }else{
-            console.log('error')
-        }
+  const [state, setState] = useState("");
+  const [woeid, setWoeid] = useState(city);
+  useEffect(() => {
+    console.log(state);
+    if (state.length > 1) {
+      axios
+        .get(
+          ` https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${state}`
+        )
+        .then((response) => console.log(response.data));
+    } else {
+      console.log("empty state");
     }
+  }, [state]);
+  return (
+    <div className="main">
+      <SideBar result={getLocation} />
+      <DetailBar value={state} />
+    </div>
+  );
+
+  async function getLocation(value) {
+    let a = await value;
+    if (a.length > 1) {
+      setState(a);
+    } else {
+      console.log("error");
+    }
+  }
 }
 
-export default Main
+export default Main;
